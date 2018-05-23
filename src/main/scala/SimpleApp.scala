ee40137f7bf7cdbcf71d2dd30ad7549eae63651b
Ivan Flores <ivan.flores@globant.com>
2018-05-23 10:13:53 -0400

    Fix problem

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -8,7 +8,7 @@ object SimpleApp {
     val spark = SparkSession.builder.appName("Simple Applicaion").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b"))-count()
+    val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
   }
