e18a51c8b37f8a24accff0b4cd1a7564ba07f162
Ivan Flores <ivan.flores@globant.com>
2018-05-23 10:24:34 -0400

    Fix problem with test

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build:
 	sbt package
 
-run: build
-	spark-submit --class "SimpleApp" --master local[4] target/scala-2.11/simple-project_2.11-1.0.jar 
+run-all: build
+	spark-submit --class "SimpleApp" --master local[4] target/scala-2.11/simple-project_2.11-1.0.jar ./files/data
+
+run:
+	spark-submit --class "SimpleApp" --master local[4] target/scala-2.11/simple-project_2.11-1.0.jar ./files/data
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -4,7 +4,8 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
   
   def main(args: Array[String]) {
-    var logFile = ""
+    var logFile = args(0)
+    println(s"Run logfile: $logFile")
     val spark = SparkSession.builder.appName("Simple Applicaion").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
