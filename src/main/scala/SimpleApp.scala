c02dc148880917f89a29612f3d55849c8df055c8
Ivan Flores <ivan.flores@globant.com>
2018-05-23 10:10:49 -0400

    Add project

---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,15 @@
+/* SimpleApp.scala */
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+  
+  def main(args: Array[String]) {
+    var logFile = ""
+    val spark = SparkSession.builder.appName("Simple Applicaion").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b"))-count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
