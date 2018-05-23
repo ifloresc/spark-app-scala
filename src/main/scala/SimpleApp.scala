826e3b97ab8d84c90e871e42309594bb5c691805
Ivan Flores <ivan.flores@globant.com>
2018-05-23 10:25:59 -0400

    format class

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -4,13 +4,23 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
   
   def main(args: Array[String]) {
+  	if (args.length == 0) {
+        println("dude, i need at least one parameter")
+    }
+
     var logFile = args(0)
+    
     println(s"Run logfile: $logFile")
+
     val spark = SparkSession.builder.appName("Simple Applicaion").getOrCreate()
+
     val logData = spark.read.textFile(logFile).cache()
+
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
+
     println(s"Lines with a: $numAs, Lines with b: $numBs")
+
     spark.stop()
   }
 }
